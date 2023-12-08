5a3c42a6ec5ca0ba34f66d7d5fc60f30be2024b4
william <william@octet.pro>
2023-12-08 18:57:34 +0800

    Optimize code

--- llama-java-app/src/main/java/chat/octet/AppStart.java
+++ llama-java-app/src/main/java/chat/octet/AppStart.java
@@ -1,22 +1,28 @@
 package chat.octet;
 
+
 import chat.octet.api.ModelBuilder;
 import chat.octet.model.Model;
 import chat.octet.model.parameters.GenerateParameter;
 import org.apache.commons.cli.*;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class AppConsole {
+@SpringBootApplication
+public class AppStart {
+
     private final static GenerateParameter DEFAULT_PARAMETER = GenerateParameter.builder().build();
     private static final Options OPTIONS = new Options();
 
     static {
         //Based parameters
         OPTIONS.addOption("h", "help", false, "Show this help message and exit.");
+        OPTIONS.addOption(null, "app", true, "App launch type: cli | api (default: cli).");
         OPTIONS.addOption("c", "completions", false, "Use completions mode.");
         OPTIONS.addOption("m", "model", true, "Load model name, default: llama2-chat.");
         //Generate parameters
@@ -74,40 +80,44 @@ public class AppConsole {
 
         if (cmd.hasOption("h") || cmd.hasOption("help")) {
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("LLAMA-JAVA-APP v1.3.0", OPTIONS);
+            formatter.printHelp("LLAMA-JAVA-APP", OPTIONS);
             System.exit(0);
         }
+        String mode = cmd.getOptionValue("app", "api");
+        if ("api".equalsIgnoreCase(StringUtils.trimToEmpty(mode))) {
+            SpringApplication.run(AppStart.class, args);
+        } else {
+            String modelName = cmd.getOptionValue("model", ModelBuilder.DEFAULT_MODEL_NAME);
+            boolean completions = cmd.hasOption("completions");
 
-        String modelName = cmd.getOptionValue("model", ModelBuilder.DEFAULT_MODEL_NAME);
-        boolean completions = cmd.hasOption("completions");
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+                 Model model = ModelBuilder.getInstance().getModel(modelName)) {
 
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-             Model model = ModelBuilder.getInstance().getModel(modelName)) {
+                GenerateParameter generateParams = parseCmdParameter(cmd);
+                String system = cmd.getOptionValue("system", "Answer the questions.");
 
-            GenerateParameter generateParams = parseCmdParameter(cmd);
-            String system = cmd.getOptionValue("system", "Answer the questions.");
+                while (true) {
+                    System.out.print("\n\nUser: ");
+                    String input = bufferedReader.readLine();
 
-            while (true) {
-                System.out.print("\n\nUser: ");
-                String input = bufferedReader.readLine();
+                    if (StringUtils.trimToEmpty(input).equalsIgnoreCase("exit")) {
+                        break;
+                    }
 
-                if (StringUtils.trimToEmpty(input).equalsIgnoreCase("exit")) {
-                    break;
+                    if (!completions) {
+                        System.out.print("AI: ");
+                        model.chat(generateParams, system, input).output();
+                    } else {
+                        System.err.print(input);
+                        model.generate(generateParams, input).output();
+                    }
+                    System.out.print("\n");
+                    model.metrics();
                 }
-
-                if (!completions) {
-                    System.out.print("AI: ");
-                    model.chat(generateParams, system, input).forEach(e -> System.out.print(e.getText()));
-                } else {
-                    System.err.print(input);
-                    model.generate(generateParams, input).forEach(e -> System.out.print(e.getText()));
-                }
-                System.out.print("\n");
-                model.metrics();
+            } catch (Exception e) {
+                System.err.println("Error: " + e);
+                System.exit(1);
             }
-        } catch (Exception e) {
-            System.err.println("Error: " + e);
-            System.exit(1);
         }
     }
 
--- llama-java-app/src/main/java/chat/octet/config/WebConfig.java
+++ llama-java-app/src/main/java/chat/octet/config/WebConfig.java
@@ -1,27 +1,20 @@
 package chat.octet.config;
 
-import com.google.common.collect.Lists;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Lists.newArrayList("*"));
-        corsConfig.setMaxAge(7200L);
-        corsConfig.addAllowedMethod("*");
-        corsConfig.addAllowedHeader("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true).maxAge(3600);
     }
-
 }
